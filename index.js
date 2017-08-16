64ed5605073980ff0498a4e2ccb05d46a8c9b403
MARIO RUIZ <elliot.ruiz82@gmail.com>
2017-08-16 02:49:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase)
+}
+
+function sayHiToGrandma(string) {
+  if string.toUpperCase === string {
+  console.log("YES INDEED!")
+  else if string === "I love you, Grandma." {
+    console.log("I love you, too.")
+  }    
+  }
+}
+
